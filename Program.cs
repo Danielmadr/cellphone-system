1f9e153cd914362c1e4fbe00e9d85fa7fe383933
Daniel <daniel.m.adr@hotmail.com>
2025-06-19 19:06:33 -0300

    Update project to .NET 9.0 and implement Iphone and Nokia classes with application installation methods

--- Program.cs
+++ Program.cs
@@ -1,3 +1,19 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Testando Nokia:");
+        Smartphone nokia = new Nokia("123456", "Nokia 3310", "111111111", 32);
+        nokia.Ligar();
+        nokia.ReceberLigacao();
+        nokia.InstalarAplicativo("Snake");
+
+        Console.WriteLine("\nTestando iPhone:");
+        Smartphone iphone = new Iphone("789012", "iPhone 13", "222222222", 128);
+        iphone.Ligar();
+        iphone.ReceberLigacao();
+        iphone.InstalarAplicativo("Instagram");
+    }
+}
